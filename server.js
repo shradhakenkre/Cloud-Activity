8f982ec266f2d8c26d85ef54ae6668aba8531d69
Savita Sidnal <savitasidnal67@gmail.com>
2025-04-05 22:59:00 +0530

    added

--- server.js
+++ server.js
@@ -9,17 +9,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// ✅ Place route handler before wildcard catch-all
+const taskRoutes = require("./routes/tasks");
+app.use("/api/tasks", taskRoutes);
+
+// ✅ Catch-all after all other routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
-
-const taskRoutes = require("./routes/tasks");
-app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
