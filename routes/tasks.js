fb14003ce5f85a8c1018c3bf7ff17d58856386c9
Savita Sidnal <savitasidnal67@gmail.com>
2025-04-05 23:06:24 +0530

    added

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+const Task = require("../models/task");
 
+// Get all tasks
 router.get("/", async (req, res) => {
   const tasks = await Task.find();
   res.json(tasks);
 });
 
-router.post('/', async (req, res) => {
-  console.log('Received new task:', req.body);
-  try {
-    const newTask = new Task({ title: req.body.title });
-    const savedTask = await newTask.save();
-    res.status(201).json(savedTask);
-  } catch (err) {
-    console.error('Error saving task:', err);
-    res.status(500).json({ message: err.message });
-  }
+// Get task by ID
+router.get("/:id", async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  res.json(task);
 });
 
+// Create a new task
+router.post("/", async (req, res) => {
+  const newTask = new Task(req.body);
+  await newTask.save();
+  res.status(201).json(newTask);
+});
+
+// Update a task
+router.put("/:id", async (req, res) => {
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTask);
+});
+
+// Delete a task
+router.delete("/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
+});
 
 module.exports = router;
